fd59dd5e17384d679f231e4c17ab4a9d0d925e4b
Thiago Rabelo <thiagorabelo1@gmail.com>
2022-09-27 22:38:43 -0300

    Exemplo de programa de linha de comando

---
+++ 17_aplicacao_de_linha_de_comando/app/app.go
@@ -0,0 +1,63 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPs e Nomes de Servidor na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca de IPs de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+	servidores, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
+}
---
+++ 17_aplicacao_de_linha_de_comando/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Ponto de partida")
+
+	aplicacao := app.Gerar()
+
+	if err := aplicacao.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
